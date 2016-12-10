c1f5b1347e4a0c2d65963cf01e5ab714a1366694
Michael Martorella <michaelmartorella@gmail.com>
2016-12-10 18:01:23 -0500

    Add logging to the server

--- api/sentinel_api_app.py
+++ api/sentinel_api_app.py
@@ -1,20 +1,21 @@
 #! /usr/bin/env python
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+
 from api_config import endpoints, push_objects
-from global_utilities.contrib.api_utilities.instapush_api import SendNotification
+from contrib.api_utilities.instapush_api import SendNotification
+
 from datetime import datetime
 import simplejson
+import os
+
+from logging.handlers import RotatingFileHandler
+import logging
 
 
 # Define the app
 app = Flask(__name__)
 api = Api(app)
-
-# add endpoints using the config file
-api.add_resource(NotifyUser, endpoints.get('notify'))
-api.add_resource(LogEvent, endpoints.get('log'))
-api.add_resource(Test, '/api/v1/test') # DEBUG
 
 
 class NotifyUser(Resource):
@@ -49,13 +50,22 @@ class LogEvent(Resource):
         # TODO - figure out how we want to log
 
 
-class Test(Resource):# DEBUG
-    def get(self):# DEBUG
-        return simplejson.dumps('Get worked!')# DEBUG
-    def post(self):# DEBUG
-        return simplejson.dumps('Post worked!')# DEBUG
+# add endpoints using the config file
+api.add_resource(NotifyUser, endpoints.get('notify'))
+api.add_resource(LogEvent, endpoints.get('log'))
 
 
 if __name__ == '__main__':
+
+    # Set up logging
+    # This obtains the log file using the LOGFILE environ variable
+    # and the destination file name
+    log_file = os.environ['LOGFILE'] + '/sentinel_api.log'
+    logging_format = '[%(asctime)s] %(levelname)-s: %(message)s'
+    handler = RotatingFileHandler(log_file, maxBytes=10485760, backupCount=20, encoding='utf8')
+    handler.setLevel(logging.DEBUG)
+    logging.basicConfig(format=logging_format, handlers=[handler])
+
+    # Start the server
     app.run(debug=True)
 
