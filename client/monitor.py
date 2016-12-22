6209060bedda45c51aa9754eed4538e994bf50df
Michael Martorella <Marto32@users.noreply.github.com>
2016-12-21 21:28:49 -0500

    Update monitor.py

--- client/monitor.py
+++ client/monitor.py
@@ -1,8 +1,11 @@
 from client.util.trigger_helper import FrontDoorMonitor
 from client.util.client_config import endpoints, gpio_pin_map, client_identifier
 
-front_door = FrontDoorMonitor('front_door', gpio_pin_map.get('front_door'),
-    endpoints.get('notify'), endpoints.get('log'))
+front_door = FrontDoorMonitor(
+    'front_door',
+    gpio_pin_map.get('front_door'),
+    endpoints.get('notify'),
+    endpoints.get('log'))
 
 if __name__ == '__main__':
     # Define the monitor_map so the client can call
