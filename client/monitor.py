0ea77acd3b866e2eb17793aaa28b1526ab595bdd
Michael Martorella <Marto32@users.noreply.github.com>
2016-12-21 21:30:32 -0500

    Syntax changes
    updated the call to `FrontDoorMonitor` to make it easier to parse.

--- client/monitor.py
+++ client/monitor.py
@@ -2,10 +2,10 @@ from client.util.trigger_helper import FrontDoorMonitor
 from client.util.client_config import endpoints, gpio_pin_map, client_identifier
 
 front_door = FrontDoorMonitor(
-    'front_door',
-    gpio_pin_map.get('front_door'),
-    endpoints.get('notify'),
-    endpoints.get('log'))
+    name='front_door',
+    gpio_pin=gpio_pin_map.get('front_door'),
+    notify_endpoint=endpoints.get('notify'),
+    log_endpoint=endpoints.get('log'))
 
 if __name__ == '__main__':
     # Define the monitor_map so the client can call
