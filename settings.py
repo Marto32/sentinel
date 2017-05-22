1408e44e8b7097c3065f15a6ce4d9d6888b70959
Michael Martorella <michaelmartorella@gmail.com>
2017-05-22 13:21:36 -0400

    Update requirements and settings files

---
+++ settings.py
@@ -0,0 +1,19 @@
+"""
+This module should house all of the configurations
+"""
+import os
+
+# Twilio Credentials
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+
+# Phone numbers must be in the format +1xxxxxxxxxx
+TWILIO_SOURCE_PHONE = os.environ.get('TWILIO_SOURCE_PHONE')
+TWILIO_DESTINATION_PHONE =os.environ.get('TWILIO_DESTINATION_PHONE')
+
+# Define the BCM pin that should be used
+PIN = os.environ.get('PIN')
+
+# Set the notification timezone
+notify_tz = os.environ.get('TIMEZONE')
+
